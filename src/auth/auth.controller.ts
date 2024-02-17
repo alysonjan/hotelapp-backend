7facb43d3016c0c1889860fcdb5354d5cd272f5b
alysonjan <alysonjan@gmail.com>
2024-02-17 21:00:53 +0800

    feature: generate jwt and authguard implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Request, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/auth.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -11,4 +12,11 @@ export class AuthController {
     signIn(@Body(ValidationPipe) signInDto: SignInDto) {
         return this.authService.signIn(signInDto)
     }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
+    }
+    
 }
